5b3f661b89c934ac2b90dd25ab207d763e02e072
Adam <aloepker@gmail.com>
2026-09-17 15:42:10 -0700

    set script code to fire and despawn the bullet

--- player/bullet_3d.gd
+++ player/bullet_3d.gd
@@ -1,1 +1,15 @@
 extends Area3D
+
+# to move the bullet:
+const SPEED = 55.0
+const RANGE = 40.0
+
+var travelled_distance = 0.0
+
+#fires the bullet on its -z axis
+func _physics_process(delta):
+	position += -transform.basis.z * SPEED * delta
+	# despawns the bullet after it travels past the RANGE value
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
